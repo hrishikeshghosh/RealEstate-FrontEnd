636fc9d9ec3f4d8d474fc81d32e684a079af9552
unknown <hrishikeshblaster@gmail.com>
2025-01-05 14:06:16 +0530

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Default 'dist', isko change karke 'build' kar diya
+  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://159.65.159.15:5000", // Backend URL
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
+
+});
