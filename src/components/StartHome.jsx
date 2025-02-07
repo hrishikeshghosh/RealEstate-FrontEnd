2c08bb8eacb5d7815c730d7688b30ef1f18497b8
jaypawar2004 <pawarjay7000@gmail.com>
2025-02-07 22:57:58 +0530

    RemoveGap

--- src/components/StartHome.jsx
+++ src/components/StartHome.jsx
@@ -324,7 +324,7 @@ const location = useLocation();
       
       <div>
         
-      <section className="py-24 relative xl:mr-0 lg:mr-5 mr-0">
+      <section className="py-24 relative -mb-[30%] lg:-mb-[15%] xl:mr-0 lg:mr-5 mr-0">
         <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
             <div className="w-full justify-start items-center xl:gap-12 gap-10 grid lg:grid-cols-2 grid-cols-1">
                 <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
